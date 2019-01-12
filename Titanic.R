de7c962762fac1cc5c148df0877c2879370d56d9
Guilherme Ferrari <guilhermefbravin@gmail.com>
2019-01-12 13:15:58 -0200

    Utilizando randomForest com alguns parametros e analisando acuracia

--- Titanic.R
+++ Titanic.R
@@ -309,16 +309,72 @@ ggplot(data.combined[1:891, ], aes(x = embarked, fill = survived)) +
   labs(fill = "Survived")
   
 
+# ---------------------
+#
+# Inicio Video 4
+#
+#----------------------
+
+library(randomForest)
+
+# Treinar o algoritmo com os parametros pclass e title
+rf.train.1 <- data.combined[1:891, c("pclass", "title")]
+rf.label <- as.factor(train$survived)
+
+set.seed(1234)
+rf.1 <- randomForest(x = rf.train.1, y = rf.label, importance = TRUE, ntree = 1000)
+rf.1
+varImpPlot(rf.1)
+
+# Treinar o algoritmo usando pclass, title e sibsp
+rf.train.2 <- data.combined[1:891, c("pclass", "title", "sibsp")]
+
+set.seed(1234)
+rf.2 <- randomForest(x = rf.train.2, y = rf.label, importance = TRUE, ntree = 1000)
+rf.2
+varImpPlot(rf.2)
+
+# Treinar usando pclass, title e parch
+rf.train.3 <- data.combined[1:891, c("pclass", "title", "parch")]
+
+set.seed(1234)
+rf.3 <- randomForest(x = rf.train.3, y = rf.label, importance = TRUE, ntree = 1000)
+rf.3
+varImpPlot(rf.3)
+
+# Combinar 2 e 3
+rf.train.4 <- data.combined[1:891, c("pclass", "title", "sibsp", "parch")]
+
+set.seed(1234)
+rf.4 <- randomForest(x = rf.train.4, y = rf.label, importance = TRUE, ntree = 1000)
+rf.4
+varImpPlot(rf.4)
 
 
+# Treinar usando pclass, title e family.size
+rf.train.5 <- data.combined[1:891, c("pclass", "title", "family.size")]
+
+set.seed(1234)
+rf.5 <- randomForest(x = rf.train.5, y = rf.label, importance = TRUE, ntree = 1000)
+rf.5
+varImpPlot(rf.5)
 
 
+# Treinar usando pclass, title, sibsp family.size
+rf.train.6 <- data.combined[1:891, c("pclass", "title", "sibsp", "family.size")]
 
+set.seed(1234)
+rf.6 <- randomForest(x = rf.train.6, y = rf.label, importance = TRUE, ntree = 1000)
+rf.6
+varImpPlot(rf.6)
 
+# Treinar usando pclass, title, parch, family.size
+rf.train.7 <- data.combined[1:891, c("pclass", "title", "parch", "family.size")]
 
-
-
-
+set.seed(1234)
+rf.7 <- randomForest(x = rf.train.7, y = rf.label, importance = TRUE, ntree = 1000)
+rf.7
+varImpPlot(rf.7)
 
 
 
